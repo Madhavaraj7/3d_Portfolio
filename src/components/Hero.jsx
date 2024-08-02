163f12d050ebf238877e78aeba2b9b8bdc8fd0be
Madhavaraj R <raj266864@gmail.com>
2024-08-02 13:46:13 +0530

    home page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,35 +1,61 @@
+import { useState, useEffect } from 'react';
 import { motion } from "framer-motion";
-
+import ReactTypingEffect from "react-typing-effect";
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
+  const [typingStarted, setTypingStarted] = useState(false);
+
+  useEffect(() => {
+    // Start typing effect after 2 seconds
+    const timer = setTimeout(() => {
+      setTypingStarted(true);
+    }, 2000); // Adjust this delay as needed
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
-        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
-        <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
-          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
 
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Madhav</span>
+          <h1 className={`${styles.heroHeadText} text-white text-5xl`}>
+            Hi, I'm{" "}
+            <span className="text-[#915EFF] text-5xl">Madhav</span>
           </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
-          </p>
+          <h2 className="text-white text-4xl mt-2">
+            {typingStarted && (
+              <ReactTypingEffect
+              text={[
+                "an Innovator of Digital Experiences",
+                "a Creator of Seamless Solutions",
+                "an Architect of Web Wonders",
+                "a Craftsman of Code",
+              ]}
+                speed={100}
+                eraseSpeed={50}
+                eraseDelay={1000}
+                typingDelay={500}
+                cursorRenderer={(cursor) => <span className="text-white">{cursor}</span>}
+              />
+            )}
+          </h2>
         </div>
       </div>
 
       <ComputersCanvas />
 
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-        <a href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
               animate={{
                 y: [0, 24, 0],
@@ -39,7 +65,7 @@ const Hero = () => {
                 repeat: Infinity,
                 repeatType: "loop",
               }}
-              className='w-3 h-3 rounded-full bg-secondary mb-1'
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
             />
           </div>
         </a>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
 import { styles } from "../styles";
 import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
@@ -13,11 +12,7 @@ const Navbar = () => {
   useEffect(() => {
     const handleScroll = () => {
       const scrollTop = window.scrollY;
-      if (scrollTop > 100) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
+      setScrolled(scrollTop > 100);
     };
 
     window.addEventListener("scroll", handleScroll);
@@ -29,9 +24,9 @@ const Navbar = () => {
     <nav
       className={`${
         styles.paddingX
-      } w-full flex items-center py-5 fixed top-0 z-20 ${
-        scrolled ? "bg-primary" : "bg-transparent"
-      }`}
+      } w-full flex items-center py-4 fixed top-0 z-20 ${
+        scrolled ? "bg-gray-900 shadow-lg" : "bg-transparent"
+      } transition-colors duration-300`}
     >
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link
@@ -42,20 +37,20 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          {/* <img src={logo} alt='logo' className='w-9 h-9 object-contain' /> */}
-          <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            MADHAV &nbsp;
-            <span className='sm:block hidden'> | MERN STACK DEVELOPER</span>
+          <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">
+            MADHAVARAJ R &nbsp;
+            <span className="sm:block hidden">| FULL STACK DEVELOPER</span>
           </p>
         </Link>
 
-        <ul className='list-none hidden sm:flex flex-row gap-10'>
+        <ul className='list-none hidden sm:flex flex-row gap-8'>
           {navLinks.map((nav) => (
             <li
               key={nav.id}
               className={`${
-                active === nav.title ? "text-white" : "text-secondary"
-              } hover:text-white text-[18px] font-medium cursor-pointer`}
+                active === nav.title ? "text-white underline" : "text-gray-300"
+              } hover:text-white text-lg font-medium cursor-pointer transition-colors duration-300`}
               onClick={() => setActive(nav.title)}
             >
               <a href={`#${nav.id}`}>{nav.title}</a>
@@ -67,22 +62,22 @@ const Navbar = () => {
           <img
             src={toggle ? close : menu}
             alt='menu'
-            className='w-[28px] h-[28px] object-contain'
+            className='w-8 h-8 object-contain cursor-pointer transition-transform duration-300 hover:scale-110'
             onClick={() => setToggle(!toggle)}
           />
 
           <div
             className={`${
               !toggle ? "hidden" : "flex"
-            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+            } p-6 bg-gray-800 absolute top-16 right-0 mx-4 my-2 min-w-[180px] z-10 rounded-lg shadow-md transition-transform duration-300`}
           >
-            <ul className='list-none flex justify-end items-start flex-1 flex-col gap-4'>
+            <ul className='list-none flex flex-col gap-4'>
               {navLinks.map((nav) => (
                 <li
                   key={nav.id}
-                  className={`font-poppins font-medium cursor-pointer text-[16px] ${
-                    active === nav.title ? "text-white" : "text-secondary"
-                  }`}
+                  className={`font-medium text-lg ${
+                    active === nav.title ? "text-white underline" : "text-gray-300"
+                  } cursor-pointer transition-colors duration-300`}
                   onClick={() => {
                     setToggle(!toggle);
                     setActive(nav.title);
